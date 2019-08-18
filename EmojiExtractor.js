a4dfe3c4e423fb1125bfdbee2be295d6f1f4910e
Lauro Moraes <lauromoraes.poa.rs@gmail.com>
2019-08-18 00:13:04 -0300

    initial release

---
+++ EmojiExtractor.js
@@ -0,0 +1,5 @@
+/**
+ * @copyright Copyright (c) 2019 Lauro Moraes - [https://github.com/subversivo58]
+ * @license The MIT License (MIT)             - [https://github.com/subversivo58/emoji-extractor/blob/master/LICENSE]
+ * @version 0.1.0 [development stage]         - [https://github.com/subversivo58/emoji-extractor/blob/master/VERSIONING.md]
+ */
